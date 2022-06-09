91ba70c6cce44486bb679f6204a8cd2118e84d23
MOAYAD <101659441+noayad@users.noreply.github.com>
2022-06-09 19:01:18 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by gabrielmaialva33 < https://github.com/gabrielmaialva33 >.
+# Copyright (C) 2021-2022 by MOAYAD < https://github.com/noayad >.
 #
-# This file is part of < https://github.com/gabrielmaialva33/winx-music-bot > project,
+# This file is part of < https://github.com/noayad/MUSIC-BOT > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/gabrielmaialva33/winx-music-bot/blob/master/LICENSE >
+# Please see < https://github.com/noayad/MUSIC-BOT/blob/master/LICENSE >
 #
 # All rights reserved.
 
